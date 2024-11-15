fa16a154ba060bface73619a012c2696aab434ea
akaMiWP <bangkokappdevelopment@gmail.com>
2024-11-15 14:20:24 +0700

    Finished hand-on

--- programs/solana-errors/src/lib.rs
+++ programs/solana-errors/src/lib.rs
@@ -7,15 +7,15 @@ declare_id!("Gp3jcr7dqCcgp3QbQdcwjS5p5n5usRLoxesQuNaHm4GD");
 pub mod solana_errors {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, count: u8) -> Result<()> {
         let data = &mut ctx.accounts.data;
 
-        data.authority = ctx.accounts.user.key();
-        data.counter = 0;
+        data.authority = ctx.accounts.user.key(); // Set to the public key of the user who signed the transaction.
+        data.counter = math_function(count).unwrap();
 
         msg!("data.conter = {}", data.counter);
-        msg!("data pubkey = {}", data.key().to_string());
-        msg!("user pubkey = {}", data.authority.key().to_string());
+        msg!("data pubkey = {}", data.key().to_string()); // public key of the data account.
+        msg!("user pubkey = {}", data.authority.key().to_string()); // public key of the user who initialized the account.
 
         Ok(())
     }
@@ -24,13 +24,15 @@ pub mod solana_errors {
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
-    user: Signer<'info>,
+    user: Signer<'info>, // Represents the user who signed the transaction.
 
     #[account(init,
         space = 8 + 32 + 1,
         payer = user,
+        seeds = [b"data"],
+        bump
     )]
-    data: Account<'info, MyData>,
+    data: Account<'info, MyData>, // Represents the data account, which will store MyData on-chain.
 
     system_program: Program<'info, System>,
 }
@@ -39,4 +41,25 @@ pub struct Initialize<'info> {
 pub struct MyData {
     authority: Pubkey,
     counter: u8,
+}
+
+#[error_code]
+pub enum MyError {
+    #[msg("invalid count value")]
+    InvalidCount,
+}
+
+fn math_function(count: u8) -> Option<u8> {
+    10u8.checked_sub(count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_math_function() {
+        assert_eq!(math_function(2), Some(8));
+        assert_eq!(math_function(11), None);
+    }
 }
--- tests/solana-errors.ts
+++ tests/solana-errors.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaErrors } from "../target/types/solana_errors";
 import { Keypair, PublicKey, LAMPORTS_PER_SOL, SystemProgram, sendAndConfirmTransaction } from '@solana/web3.js';
+import { assert } from "chai";
 
 describe("solana-errors", () => {
 
@@ -10,22 +11,48 @@ describe("solana-errors", () => {
 
   const program = anchor.workspace.SolanaErrors as Program<SolanaErrors>;
   const user = Keypair.generate();
-  const data = Keypair.generate();
+  const [data] = PublicKey.findProgramAddressSync([Buffer.from("data")], program.programId)
+
+  before("prepare", async () => {
+    await airdrop(anchor.getProvider().connection, user.publicKey)
+  })
 
   it("Is initialized!", async () => {
-
     console.log("user balance = " + await connection.getBalance(user.publicKey))
     const tx = await program.methods
-      .initialize()
+      .initialize(1)
       .accountsStrict({
         user: user.publicKey,
-        data: data.publicKey,
+        data: data,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([user])
+      .rpc({ commitment: "confirmed"});
+
+      let dataAccount = await program.account.myData.fetch(data)
+      assert.deepEqual(dataAccount.authority, user.publicKey)
+      assert.deepEqual(dataAccount.counter, 9)
+  });
+
+  it("Is not initialized with incorrect data account!", async () => {
+    const bad_data_account = Keypair.generate(); // not a PDA
+
+    try {
+      const tx = await program.methods
+      .initialize(1)
+      .accountsStrict({
+        user: user.publicKey,
+        data: bad_data_account.publicKey,
         systemProgram: SystemProgram.programId
       })
       .signers([user])
       .rpc();
 
-    console.log("Your transaction signature", tx);
+      assert.fail("Should not pass")
+    } catch(_err) {
+      const err = anchor.AnchorError.parse(_err.logs);
+      assert.strictEqual(err.error.errorCode.code, "ConstraintSeeds");
+    }
   });
 });
 
@@ -33,3 +60,4 @@ describe("solana-errors", () => {
 async function airdrop(connection: any, address: any, amount = 1000000000) {
   await connection.confirmTransaction(await connection.requestAirdrop(address, amount), "confirmed");
 }
+ 
